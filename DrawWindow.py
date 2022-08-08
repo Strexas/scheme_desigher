e0681e35c2df081eca75cf7e99791a0c187b2272
Strexas <kirsnauskas@bk.ru>
2022-08-08 16:34:08 +0500

    add 'circle' and 'rect' functions fix some bugs

--- DrawWindow.py
+++ DrawWindow.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame as pg
 
 window = pg.display.set_mode((1000, 500))
@@ -12,6 +14,8 @@ class DrawWindow:
         self.prev_mouse = (0, 0, 0, 0, 0)
         self.font = pg.font.SysFont('latohairline', 15)
         self.line_begin = False
+        self.rect_begin = False
+        self.circle_begin = False
 
     def draw(self):
         window.blit(self.surface, (0, 30))
@@ -35,6 +39,29 @@ class DrawWindow:
                     else:
                         self.line_begin = pos
 
+                if self.button == 'rect':
+                    if self.rect_begin:
+                        w, h = abs(pos[0] - self.rect_begin[0]), abs(pos[1] - self.rect_begin[1])
+                        x_b, y_b = min(pos[0], self.rect_begin[0]), min(pos[1], self.rect_begin[1])
+                        pg.draw.rect(self.surface, 'black', (x_b, y_b - 30, w, h), 1)
+                        self.rect_begin = False
+                    else:
+                        self.rect_begin = pos
+
+                if self.button == 'circle':
+                    if self.circle_begin:
+                        r = math.sqrt(pow(pos[0] - self.circle_begin[0], 2) + pow(pos[1] - self.circle_begin[1], 2))
+                        pg.draw.circle(
+                            self.surface,
+                            'black',
+                            (self.circle_begin[0], self.circle_begin[1] - 30),
+                            min(r, self.circle_begin[1] - 30),
+                            1
+                        )
+                        self.circle_begin = False
+                    else:
+                        self.circle_begin = pos
+
         self.prev_mouse = pg.mouse.get_pressed()
 
         if pos[1] >= 40:
@@ -45,5 +72,20 @@ class DrawWindow:
             window.blit(text, rect)
             pg.draw.rect(window, 'black', rect, 1)
 
-            if self.line_begin:
+            if self.line_begin and self.button == 'line':
                 pg.draw.line(window, 'black', self.line_begin, pos, 1)
+
+            if self.rect_begin and self.button == 'rect':
+                w, h = abs(pos[0] - self.rect_begin[0]), abs(pos[1] - self.rect_begin[1])
+                x_b, y_b = min(pos[0], self.rect_begin[0]), min(pos[1], self.rect_begin[1])
+                pg.draw.rect(window, 'black', (x_b, y_b, w, h), 1)
+
+            if self.circle_begin and self.button == 'circle':
+                r = math.sqrt(pow(pos[0] - self.circle_begin[0], 2) + pow(pos[1] - self.circle_begin[1], 2))
+                pg.draw.circle(
+                    window,
+                    'black',
+                    (self.circle_begin[0], self.circle_begin[1]),
+                    min(self.circle_begin[1] - 30, r),
+                    1
+                )
--- ToolBox.py
+++ ToolBox.py
@@ -7,7 +7,7 @@ class ToolBox:
     def __init__(self):
         self.surface = pg.Surface((500, 30))
         font = pg.font.SysFont('latohairline', 20)
-        self.texts = ['hand', 'point', 'line']
+        self.texts = ['hand', 'point', 'line', 'rect', 'circle']
         self.text_buttons = [font.render(i.ljust(6, ' '), False, 'white') for i in self.texts]
         self.text_rects = [i.get_rect() for i in self.text_buttons]
         self.text_rects[0].topleft = (5, 0)
@@ -20,7 +20,7 @@ class ToolBox:
         pg.draw.rect(window, 'blue', self.text_rects[0], 1)
 
     def render(self):
-        for i in range(3):
+        for i in range(len(self.texts)):
             self.surface.blit(self.text_buttons[i], self.text_rects[i])
             window.blit(self.surface, (0, 0))
 
@@ -28,7 +28,7 @@ class ToolBox:
         mp = pg.mouse.get_pressed()
         if mp[0]:
             pp = pg.mouse.get_pos()
-            for i in range(3):
+            for i in range(len(self.texts)):
                 if self.text_rects[i].collidepoint(pp):
                     self.pressed_button.button = self.texts[i]
                     pg.event.post(self.pressed_button)
